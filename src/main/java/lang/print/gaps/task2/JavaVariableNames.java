fa12d2e24689063aa628fcdc9cad76b0a026e819
rustamkremcheev <rustam.omg1996@gmail.com>
2022-11-13 15:43:20 +0500

    This is very exciting for me -_-

--- src/main/java/lang/print/gaps/task2/JavaVariableNames.java
+++ src/main/java/lang/print/gaps/task2/JavaVariableNames.java
@@ -3,12 +3,12 @@ package lang.print.gaps.task2;
 public class JavaVariableNames {
     public static void main(String[] args) {
         int tomorrow = 0;
-        int $money = 10;
-        int one_hundred = 100;
+        int money = 10;
+        int oneHundred = 100;
         int back = 1000;
-        int _underscore = 10000;
+        int underscore = 10000;
         int sharp = 100000;
         int star = 1000000;
-        int BAD_EXAMPLE_OF_VAR_NAME = 1000000;
+        int badExampleOfVarName = 1000000;
     }
 }
